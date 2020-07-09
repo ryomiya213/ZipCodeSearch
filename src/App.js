c06e837bf698a58d5b138eec5055167401befde7
Ryo Miya <ryomiya213@gmail.com>
2020-07-09 22:04:39 +0900

    「以下に掲載がない場合」見やすく。それに伴うリファクタリング

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ async function App() {
 
   const zipCodeForm = document.querySelector('#zipCodeForm');
   const zipCodeInputElement = document.querySelector('#zipCode');
-  const addressElement = document.querySelector('#address');
 
   zipCodeForm.addEventListener(('submit'), (event) => {
     event.preventDefault();
-    addressElement.innerHTML = '';
+    zipCodeForm.nextElementSibling.remove();
     const zipCode = zipCodeInputElement.value;
     if (/\d{7}/.test(zipCode)) {
       const addressList = [];
@@ -19,37 +18,66 @@ async function App() {
           addressList.push(address);
         }
       });
-      if (addressList.length === 0) {
-        addressElement.innerHTML = "みつかりませんでした"
-      } else {
-        addressList.forEach(address => {
-          switch (true) {
-            case address.Street.endsWith('以下に掲載がない場合'):
-              addressElement.innerHTML += `住所: ${address.Prefecture} ${address.City} 以下に掲載がない地域` + '<br>';
-              zipCodeData.forEach(masterData => {
-                if (address.City === masterData.City && address.zipCode !== masterData.zipCode) {
-                  addressElement.innerHTML += ` ${masterData.Street}`;
-                }
-              });
-              addressElement.innerHTML += '<br>';
-              break;
-            case address.Street.endsWith('の次に番地がくる場合'):
-              addressElement.innerHTML += `住所: ${address.Prefecture} ${address.City} 次に番地がくる地域` + '<br>';
-              break;
-            case address.Street.endsWith('一円'):
-              addressElement.innerHTML += `住所: ${address.Prefecture} ${address.City}` + '<br>';
-              break;
-            default:
-              addressElement.innerHTML += `住所: ${address.Prefecture} ${address.City} ${address.Street}` + '<br>';
-          }
-        });
-      }
-
+      zipCodeForm.after(makeAdressHTML(addressList, zipCodeData));
     } else {
-      addressElement.innerHTML = "7桁の数字で入力してください"
+      zipCodeForm.after(makeErrorHTML('7桁の数字で入力してください'));
     }
     
   });
+}
+
+/**
+ * 住所を表示するHTML要素を作成する。
+ * @param {Array} addressList 表示したい住所の一覧
+ * @param {Array} zipCodeData 全ての郵便番号の一覧
+ * @returns {HTMLElement}
+ */
+function makeAdressHTML(addressList, zipCodeData) {
+  const addressElement = document.createElement('div');
+  addressElement.classList.add('address');
+  let addressHTML = '';
+  if (addressList.length === 0) {
+    const PElement = document.createElement('p');
+    PElement.innerHTML = "みつかりませんでした";
+    addressElement.appendChild(PElement);
+  } else {
+    addressList.forEach(address => {
+      const PElement = document.createElement('p');
+      switch (true) {
+        case address.Street.endsWith('以下に掲載がない場合'):
+          PElement.innerHTML = `住所: ${address.Prefecture} ${address.City} 以下に掲載がない地域`;
+          const noIncludesElement = document.createElement('div');
+          noIncludesElement.classList.add('noIncludes');
+          zipCodeData.forEach(masterData => {
+            if (address.City === masterData.City && address.zipCode !== masterData.zipCode) {
+              noIncludesElement.innerHTML += ` ${masterData.Street}`;
+            }
+          });
+          PElement.appendChild(noIncludesElement);
+          break;
+        case address.Street.endsWith('の次に番地がくる場合'):
+          PElement.innerHTML = `住所: ${address.Prefecture} ${address.City} 次に番地がくる地域`
+          break;
+        case address.Street.endsWith('一円'):
+          PElement.innerHTML = `住所: ${address.Prefecture} ${address.City}`
+          break;
+        default:
+          PElement.innerHTML = `住所: ${address.Prefecture} ${address.City} ${address.Street}`;
+      }
+      addressElement.appendChild(PElement);
+    });
+  }
+  return addressElement;
+}
+
+/**
+ * エラーを表示するHTML要素を作成する
+ * @param {String} error エラー内容
+ */
+function makeErrorHTML (error) {
+  const errorElement = document.createElement('p');
+  errorElement.innerHTML = error;
+  return errorElement;
 }
 
 
